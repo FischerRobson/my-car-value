09c2e53f864ce4ab1eec396734d25c8a3c39709c
Fischer <fischerrobson@gmail.com>
2023-03-14 20:48:21 -0300

    user service with injected repository

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDTO } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 @Controller('auth')
 export class UsersController {
+
+  constructor(private service: UsersService) { 
+  }
 
   @Post('/signup')
   async createUser(@Body() body: CreateUserDTO) { 
     const { email, password } = body;
+
+    this.service.create(email, password);
   }
 }
